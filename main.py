72b2ee1c04fafc6efc0bf338462cb7044a6b4ae5
ameer <ameermohammadramzani@gmail.com>
2021-05-24 17:23:09 +0430

    Counselor, Group and Subgroups were added.

--- main.py
+++ main.py
@@ -34,3 +34,47 @@ class User(db.Model):
         self.phone_number = phone_number
         self.email = email
         self.age = age
+
+
+class Counselor(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    phone_number = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    degree = db.Column(db.String(255))
+    score = db.Column(db.Integer(), default=0)
+
+    def __init__(self, username, firstname, lastname, phone_number, email, password, degree, score=0):
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+        self.phone_number = phone_number
+        self.email = email
+        self.password = password
+        self.degree = degree
+        self.score = score
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    subgroups = db.relationship('Subgroup', backref='group', lazy='dynamic')
+
+    def __init__(self, title):
+        self.title = title
+
+
+class Subgroup(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    group_foreignkey = db.Column(db.Integer(), db.ForeignKey('group.id'))
+
+    def __init__(self, title):
+        self.title = title
+
+# un1 = User('aaa', '123', 'what', 'what', '9879879876', 'afds@asdf.asdf', 22)
+# cn1 = Counselor('conone', 'conA', 'conB', 'conNum', 'conMail', 'conPass', 'conDe')
+
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
-from main import app, db, User
+from main import app, db, User, Counselor, Group, Subgroup
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Counselor=Counselor, Group=Group, Subgroup=Subgroup)
